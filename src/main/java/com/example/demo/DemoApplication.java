2ef4a9231c8f84367fe66738b4c2882d8f4125d8
kurular4 <kurular4@gmail.com>
2021-08-30 20:10:16 +0300

    Todos added for refactor

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+// todo convert fields injections to constructor inj.
+// todo return ResponseEntity instead of custom response model
+// todo convert constant classes to enum
+// todo use own jwt util lib
+// todo refine authentication/authorization structure
+
 @SpringBootApplication
 @EnableJpaRepositories
 @ServletComponentScan
